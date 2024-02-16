2e71050d4e4ef8c57786168b468b88715fd32353
Farhan <48715452+FrhCode@users.noreply.github.com>
2024-02-16 19:28:34 +0700

    update default value

--- Security.Api/Options/SmtpOption.cs
+++ Security.Api/Options/SmtpOption.cs
@@ -2,9 +2,9 @@
 
 public class SmtpOption
 {
-	public string Host { get; set; } = String.Empty;
-	public string Username { get; set; } = String.Empty;
-	public string Password { get; set; } = String.Empty;
+	public string Host { get; set; } = string.Empty;
+	public string Username { get; set; } = string.Empty;
+	public string Password { get; set; } = string.Empty;
 	public int Port { get; set; }
 	// public string From { get; set; } = String.Empty;
 	// public bool UseSsl { get; set; }
